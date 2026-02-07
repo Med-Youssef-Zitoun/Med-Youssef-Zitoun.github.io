b9a3659bcdb72c4bce53724652ec5205ecd49f54
Med Youssef Zitoun <mohamedyoussefzitoun@gmail.com>
2026-02-07 15:27:23 +0100

    Version 1.4

--- Script.js
+++ Script.js
@@ -1,25 +1,16 @@
-var myVar;
-
 function loader() {
-    myVar = setTimeout(showPage, 3000);
+    document.body.classList.add("loading");
+    myVar = setTimeout(showPage, 2000);
     goTopBtn.style.display = "none";
 }
 
 function showPage() {
     document.getElementById("loader").style.display = "none";
     document.getElementById("All").style.display = "block";
-    document.getElementById(window.location.href.split("#")[1]).scrollIntoView();
+    document.body.classList.remove("loading");
 
-    /*==================== scroll reveal ====================*/
-    ScrollReveal({
-        //reset: true,
-        distance: '80px',
-        duration: 2000,
-        delay: 200
-    });
-
-    ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
-    ScrollReveal().reveal('.home-img, .portfolio-box, .contact, .form', { origin: 'bottom' });
-    ScrollReveal().reveal('.home-content h1', { origin: 'left' });
-    ScrollReveal().reveal('.home-content p', { origin: 'right' });
+    const id = window.location.href.split("#")[1];
+    if (id) {
+        document.getElementById(id).scrollIntoView();
+    }
 }
